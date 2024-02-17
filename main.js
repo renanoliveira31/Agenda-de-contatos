11f27caeade382647272b8f738ba9501f35e7d3e
Renan Oliveira <renanfeoliveira1@gmail.com>
2024-02-17 17:27:40 -0300

    Feito a parte de HTML, CSS e JS
    Agenda feita através de HTML, CSS e JS. Ao preencher os campos e clicar em "cadastrar" o individuo irá receber seu nome e número em uma tabela abaixo.
    Também caso não preencha de acordo o nome completo ou número não será possiel enviar o formulário

---
+++ main.js
@@ -0,0 +1,53 @@
+const form = document.getElementById('formulário-agenda');
+const nomes = [];
+const numeros = [];
+let linhas = '';
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputNumero = document.getElementById('telefone');
+
+    if(nomes.includes(inputNome.value)){
+        alert(`O nome: ${inputNome.value} já foi inserido`);
+    } else {
+        nomes.push(inputNome.value);
+        numeros.push(inputNumero.value);
+    
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputNumero.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+    
+    inputNome.value = '';
+    inputNumero.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function converterParaMaiusculas(elemento) {
+    elemento.value = elemento.value.toUpperCase();
+}
+
+function validarFormulario() {
+    var nomeCompleto = document.getElementById('nome').value;
+    var partesNome = nomeCompleto.split(' ');
+    
+    if (partesNome.length < 2) {
+        alert("Por favor, insira o nome completo");
+        return false; 
+    }
+    return true; 
+}
